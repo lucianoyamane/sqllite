b506b401827dfd4824215e9b40283acecad60f14
luciano <inside@gmail.com>
2019-05-22 14:47:32 -0300

    adicionado projeto

---
+++ demosqlite/src/main/java/com/example/sqllite/demosqlite/Configuration/DBConfig.java
@@ -0,0 +1,19 @@
+package com.example.sqllite.demosqlite.Configuration;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DBConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.sqlite.JDBC");
+        dataSourceBuilder.url("jdbc:sqlite:chinook.db");
+        return dataSourceBuilder.build();
+    }
+}
---
+++ demosqlite/src/main/java/com/example/sqllite/demosqlite/DemosqliteApplication.java
@@ -0,0 +1,13 @@
+package com.example.sqllite.demosqlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosqliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemosqliteApplication.class, args);
+	}
+
+}
